f1928083c33a62e7c49d80300e8c3ca9cbdb84d9
OksanaGriyo <xxmargoxx@gmail.com>
2013-09-02 08:04:06 +0300

    Moderator page (not ready)

---
+++ src/edu/vntu/mblog/web/ModeratorServlet.java
@@ -0,0 +1,92 @@
+package edu.vntu.mblog.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import javax.servlet.http.HttpSession;
+
+import edu.vntu.mblog.domain.User;
+import edu.vntu.mblog.errors.UserNotFoundException;
+//import edu.vntu.mblog.domain.User;
+//import edu.vntu.mblog.errors.UserNotFoundException;
+import edu.vntu.mblog.errors.ValidationException;
+import edu.vntu.mblog.services.PostsService;
+//import edu.vntu.mblog.services.UsersService;
+
+/**
+ * 
+ */
+@WebServlet("/moderator")
+public class ModeratorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private static final int POSTS_LIMIT = 100;
+	
+	//private final UsersService usersService = UsersService.getInstance();
+	private final PostsService postsService = PostsService.getInstance();
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	getServletContext().log("Rendering moderator page");
+    	
+    	try {
+			request.setAttribute("posts", postsService.getAllPosts(0, POSTS_LIMIT));
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+    	
+        RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/jsp/moderator_posts_page.jsp");
+        view.forward(request, response);
+	}
+	
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doPost(req, resp);
+//		String login = req.getParameter("msg.authorLogin");
+//		String text = req.getParameter("msg.text");
+//		
+//		
+//		
+//		try {
+//			postsService.confirmPost(login, text);
+//			
+//			resp.sendRedirect(req.getContextPath() + "/moderator");
+//		
+//		}
+//		catch (UserNotFoundException unfe) {
+//			unfe.printStackTrace();
+//			// TODO: show error page here
+//		}
+//		catch (ValidationException e) {
+//			// TODO and this
+//			e.printStackTrace();
+//		}
+//		 
+	}
+
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doDelete(req, resp);
+		
+	}
+	
+	
+	
+	
+	
+
+
+}
---
+++ web/WEB-INF/jsp/moderator_posts_page.jsp
@@ -0,0 +1,54 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="auth"  uri="http://vntu.edu.ua/jsp/taglib/permchecker" %>
+
+<%@include file="includes/header.jsp" %>
+<%@include file="includes/navigation.jsp" %>
+
+
+<div class="container">
+		
+ 	<!-- Messages -->
+	<div class="row-fluid">
+	
+		<c:forEach var="msg" items="${posts}">
+
+				    <div class="row-fluid">
+				    	<div class="span1 text-center">
+				    		<a href="${pageContext.request.contextPath}/users/${msg.authorLogin}/">
+				    			<img src="http://placehold.it/42x42" class="img-circle">
+			    			</a>
+				    	</div>
+			  		    <div class="span9"><c:out value="${msg.text}"/></div>
+					    <div class="span2 timebadge">
+					    	<span class="badge pull-right">
+					    		<i class="icon-time"></i> <fmt:formatDate type="both" dateStyle="short" timeStyle="short" value="${msg.date}"/>
+					    	</span>
+					    </div>
+<!-- 					    <div class="pull-right"> -->
+<!-- 					    	<button type="button" class="btn btn-danger btn-mini"><i class="icon-ok"></i> Підтвердити </button> -->
+<!-- 		               		<button type="button" class="btn btn-success btn-mini"><i class="icon-remove"></i> Заборонити </button> -->
+<!-- 					    </div> -->
+			    	    
+			    	    
+				    	<form action="${pageContext.request.contextPath}/moderator" method="DELETE">
+							<button type="submit" class="btn btn-danger btn-mini pull-right"> <i class="icon-remove"></i> Заборонити</button>
+						</form>
+				  
+				   
+		    			<form action="${pageContext.request.contextPath}/moderator" method="POST">
+		        			<button type="submit" class="btn btn-success btn-mini pull-right"> <i class="icon-ok"></i> Підтвердити</button>
+	        			</form>
+				       
+			    	    
+				    </div>
+				    <hr/>
+
+	    </c:forEach>
+	    
+	</div><!-- .row-fluid -->
+	
+</div><!-- .container -->
+
+<%@include file="includes/footer.jsp" %>
