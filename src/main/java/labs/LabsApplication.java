81ef319d39f1cb62258720d62097c20b2f87725a
Sergey Khruschak <sergey.khruschak@gmail.com>
2015-07-12 22:44:51 +0300

    Project initialized, added index page and controller, spring security is turned off temporarilly

---
+++ src/main/java/labs/LabsApplication.java
@@ -0,0 +1,12 @@
+package labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabsApplication.class, args);
+    }
+}
